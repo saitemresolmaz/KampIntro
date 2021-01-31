299865a35fe7b8cbbe3b1e733fb5a789c9fa1a39
saitemresolmaz <76459268+saitemresolmaz@users.noreply.github.com>
2021-01-31 03:30:18 +0300

    Abstract Class

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Abstract class new'lenemez.
+            //Database database = new Database();
+            Database database1 = new MySql();
+            database1.Add();
+
+            Database database2 = new Oracle();
+            database2.Add();
+
+
+            database1.Update();
+            database2.Update();
+
+        }
+    }
+
+    abstract class Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Veritabanına eklendi");
+        }
+        public abstract void Update();
+
+    }
+
+    class MySql : Database
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Updated by Sql");
+        }
+    }
+
+    class Oracle : Database
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Updated by Oracle");
+        }
+    }
+}
